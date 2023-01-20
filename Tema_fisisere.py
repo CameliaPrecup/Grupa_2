25b2c5a4c690abb9a25fd0e5f331e6929fe9c8f5
CameliaPrecup <precup_cami@yahoo.com>
2023-01-20 17:20:33 +0200

    Tema_fisiere

--- Tema_fisisere.py
+++ Tema_fisisere.py
@@ -11,33 +11,34 @@ with open('categorii.txt' ,'a' ) as file:
 
 
 lista_categorii=[]
-with open('categorii.txt, r') as file:
+with open('categorii.txt', 'r') as file:
     while True:
         line=file.readline().replace('\n',"")
         if not line:
          break
         lista_categorii.append(line)
 
-def validate_category(categorie):
+def validate_categorii(categorie):
     global lista_categorii
     if categorie not in lista_categorii:
-        return False
-    print('categoria nu exista, alegeti din categoriile de mai jos')
-    print(lista_categprii)
+        # return False
+     print('Categoria nu exista.Introduceti alta categorie :')
+    print(lista_categorii)
+    return False
 
     return True
 
-    return False
+    # return False
 
-# def validate_date(date_string):
-#     try:
-#         date= dateline.datetime.strpline('date_string, %Y-%m-%d')
-#     except Exception as e :
-#         print('Data introdusa nu este in formatul corect(yyyy-ll-dd)!')
-#     return
-#     False
-#     else:
-#     return True
+def validate_date(date_string):
+    try:
+        date= datetime.datetime.strptime('date_string, %Y-%m-%d')
+    except Exception as e :
+        print('Data introdusa nu este in formatul corect(yyyy-ll-dd)!')
+    return False
+    # False
+    else :
+     return True
 
 # def validate_category(categorie):
 #     global lista_categorii
@@ -50,15 +51,15 @@ def validate_category(categorie):
 
 with open('taskuri.csv ,a') as file:
     while True:
-     tasks =input("Introduceti un task")
+     tasks =input("Introduceti un task.Tastati enter pentru a incheia")
     if task == '':
         break
     while True:
-     end_date=('Introduceti data limita')
+     end_date=('Introduceti data limita:')
     if validate_date(end_date):
-        break
-    if end_date=='':
-        break
+        # break
+    # if end_date=='':
+    #     break
     responsible= input('Introduceti o persoana responsabila:')
     if responsible=='':
         break
@@ -66,7 +67,11 @@ with open('taskuri.csv ,a') as file:
      categorie=input('Introduceti o categorie:')
     if validate_categorii(categorie):
         break
-    next_category=input('Introduceti alta categorie?Y/N):')
-    csv.writer(file , delimiter= ',')
-    csv_writer.writerow([task,end_date,resposible,category])
-    next_task =input('Introduceti un alt task Y/N')
+    next_category=input('Introduceti alta categorie?(Y/N):')
+    csv.writer=csv.writer(file , delimiter= ',')
+    csv_writer.writerow([tasks,end_date,resposible,category])
+    if next_category.lower!='Y':
+        break
+    end_date=datetime.datetime.strptime(input('Introduceti o data limita(YYYY-MM-DD):'), '%Y-%m-%m')
+
+    # next_task =input('Introduceti un alt task Y/N')
